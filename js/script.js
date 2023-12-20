c9a8163f95a7867f9b70ba5857264dbbf9e05ed3
Lukw07 <kry.tuma@gmail.com>
2023-12-20 13:23:43 +0100

    mobile responsivity

--- js/script.js
+++ js/script.js
@@ -10,3 +10,15 @@
       }
     });
   })(jQuery);
+  let count = 0;
+    const gap = 45;
+    const flipElement = document.getElementById("flip");
+    setInterval(function() {
+      if (count == flipElement.children.length-1) count = 0;
+      else count++;
+      size = 0;
+      for (let i = 0; i < count; i++) {
+        size += flipElement.children[i].offsetHeight+gap;
+      }
+      flipElement.children[0].style.marginTop = `${-size}px`;
+    }, 5000);
