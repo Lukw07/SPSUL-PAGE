3946c959f8f191978fd4af119d01c1675bd54da7
Lukw07 <kry.tuma@gmail.com>
2024-01-04 13:19:57 +0100

    edit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-(function($) {
-    $('.js-nav-menu-toggle').on('click', function() {
-      $(this).parents('.navigation-menu').toggleClass('navigation-menu--open');
-    });
-    
-    $('html').on('click', function(e) {
-      if(!$(e.target).closest('.js-nav-menu').length &&
-        ($('.js-nav-menu').hasClass('navigation-menu--open'))) {
-          $('.js-nav-menu').removeClass('navigation-menu--open');
-      }
-    });
-  })(jQuery);
   let count = 0;
     const gap = 45;
     const flipElement = document.getElementById("flip");
